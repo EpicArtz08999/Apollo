68ebe1417922fb57a4ed8ae929e85163007d03aa
caspervanneck <nethercube.host@gmail.com>
2016-08-28 20:04:31 +0200

    Update Server.php

--- src/pocketmine/Server.php
+++ src/pocketmine/Server.php
@@ -170,6 +170,7 @@ use pocketmine\utils\TextFormat;
 use pocketmine\utils\Utils;
 use pocketmine\utils\UUID;
 use pocketmine\utils\VersionString;
+use pocketmine\updater\AutoUpdater;
 
 use synapse\Synapse;
 
